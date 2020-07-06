7a5c730ba7edcdacdd7bc7b50adf97f93f000c91
AkiroPhi <akirophi@gmail.com>
2020-07-06 17:26:11 +0200

    merge fix

--- conf/report.py
+++ conf/report.py
@@ -21,5 +21,8 @@ CVSS = {"AV", "AC", "PR", "UI", "S", "C", "I", "A"}
 HISTORIES = {"reco", "observPos", "observNeg", "risk"}
 
 GIT = "git@127.0.0.1:~/vulnerabilities.git"
+GIT = "git@github.com:AkiroPhi/vulnerabilities.git"
+GIT = "https://github.com/AkiroPhi/vulnerabilities.git"
 SSH_KEY = "~/.ssh/id_rsa"
 REFRESH_RATE = 10
+COMMIT_MESSAGE = "Commit auto"
